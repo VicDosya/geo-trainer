01c99d82e00e876a8e9f9851fb43ca538ea4b7b5
Dosya <96661032+VicDosya@users.noreply.github.com>
2022-10-24 16:41:06 +0300

    Object response (#3)
    * Add object response from the server and apply to JSX
    
    * Add choices in the flag quiz
    
    * Remove useless state variables

--- client/src/components/Trainer.js
+++ client/src/components/Trainer.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import styles from './Trainer.module.css';
-import picture from './placeholderimg.png';
 
 export const Trainer = () => {
+
+    //useState variables
+    const [question, setQuestion] = useState([]);
+
+    //Fire loadQuiz function when the page loads. The page rendered twice(I removed StrictMode from index.js).
+    useEffect(() => {
+        loadQuiz();
+    }, []);
+
+    //Load quiz(question) function
+    const loadQuiz = async () => {
+        const res = await axios.get('/api/quiz');
+        setQuestion(res.data);
+    };
 
     //Redirecting to home page on button click
     let navigate = useNavigate();
     const goBackRoute = () => navigate(-1);
 
+    //JSX
     return (
         <div>
             <div className={styles.topContainer}>
@@ -16,15 +31,15 @@ export const Trainer = () => {
                 <h1 className={styles.trainerTitle}>Trainer Page</h1>
             </div>
             <img className={styles.imgContainer}
-                src={picture}
-                alt="An example picture">
+                src={question.flagImgUrl}
+                alt="Question image">
             </img>
-
+            <div className={styles.questionContainer}>
+                <h1 className={styles.questionTitle}>{question.question}</h1>
+            </div>
             <div className={styles.choicesContainer}>
-                <button className={styles.answer}>Answer 1</button>
-                <button className={styles.answer}>Answer 2</button>
-                <button className={styles.answer}>Answer 3</button>
-                <button className={styles.answer}>Answer 4</button>
+                {question.options?.map((option, index) => <button key={index} className={styles.answer}>{option}</button>)}
+                {/* Optional Chaining (the question mark) used here, because JS thought the question.options array was undefined. */}
             </div>
         </div>
     )
