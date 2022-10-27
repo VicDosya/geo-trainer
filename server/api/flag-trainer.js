d811fb51e58bbe799bd12df736007b8ab13ee45b
dosya <vicdosya@gmail.com>
2022-10-28 00:35:22 +0300

    Add and randomize choices with correct and wrong answers

--- server/api/flag-trainer.js
+++ server/api/flag-trainer.js
@@ -1,28 +1,62 @@
 const express = require('express');
+const { countriesData } = require('countries-data-all');
 const app = express();
+
+//Constant Variables
+const MAX_RANDOM_FLAG_RETRY_COUNT = 5;
+
+//Functions
+//Randomizer index variable for the CORRECT flag!
+let correctFlagNumber = Math.floor(Math.random() * 242) + 1;
+
+//Randomizer index for RANDOM(incorrect) flag
+let randomFlagNumber = (retryCount = 0) => {
+  let randomFlag = Math.floor(Math.random() * 242) + 1;
+
+  //Generate a randomFlag again if duplicates are detected, stop a recursive function with retryCount.
+  if (randomFlag === correctFlagNumber && retryCount < MAX_RANDOM_FLAG_RETRY_COUNT) {
+    randomFlagNumber();
+    retryCount++
+  } else if (retryCount >= MAX_RANDOM_FLAG_RETRY_COUNT) {
+    return console.log(`Retry count has reached the limit of ${MAX_RANDOM_FLAG_RETRY_COUNT}`);
+  } else {
+    return randomFlag;
+  }
+};
 
 //Routes:
 app.get('/', (req, res) => {
   res.send({ routeName: "Hello World" });
 });
 
+//Sending flag information from countries-data-all package to the client
 app.get('/quiz', (req, res) => {
+
+  //Send back flag information to the client.
   res.send({
     id: 1,
     question: "What flag is this?",
-    flagImgUrl: "https://upload.wikimedia.org/wikipedia/commons/d/d4/Flag_of_Israel.svg",
+    flagImgUrl: countriesData[correctFlagNumber].flag,
+
     options: [
-      "United Kingdom",
-      "United States",
-      "Israel",
-      "Zimbabwe"
-    ],
-    answer: "Israel"
+      countriesData[randomFlagNumber()].country,
+      countriesData[randomFlagNumber()].country,
+      countriesData[correctFlagNumber].country,
+      countriesData[randomFlagNumber()].country
+    ].sort((a, b) => 0.5 - Math.random()),   //Randomise the positioning of the choices
+
+    answer: countriesData[correctFlagNumber].country
   });
 });
 
+//Client's guess handling.
 app.post('/guess', (req, res) => {
-  res.send({ routeName: "Hello Guess" });
+  if (req.body.userGuess === countriesData[correctFlagNumber].country) {
+    res.send({ returnStatus: "Correct!", correctChoice: true });
+    correctFlagNumber = Math.floor(Math.random() * 242) + 1;
+  } else {
+    res.send({ returnStatus: "Wrong" });
+  }
 });
 
 module.exports = app;
